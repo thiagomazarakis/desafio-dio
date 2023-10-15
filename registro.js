1bf78ca29db1efd78c222dbeccc3edff5cf1387d
thiagomazarakis <thiagomazarakiss.rj@gmail.com>
2023-10-15 00:05:26 -0300

    meu primeiro commit

---
+++ registro.js
@@ -0,0 +1,16 @@
+const registro = ["Nome Completo ", "CPF " , "RG " , "Data de Nascimento " , "Sexo "]
+
+let dados = [
+   [ "Carla Souza Paris" , "885.467.657-60" , "25.657.620-20", "20/05/1951" , "Feminino"],
+   [ "Paulo de Carvalho", "775.467.657-51" , "36.657.620-33" , "11/06/1985" , "Masculino"],
+   [ "Beatriz de Souza Barão", "665.467.657-45" , "48.657.620-40" , "29/05/1970" , "Feminino"],
+   [ "Pedro Cruz da Silva", "555.467.657-32" , "59.657.620-58" , "20/07/1952" , "Masculino"],
+   [ "Manuele Pedrosa de Souza", "445.467.657-27" , "34.657.620-62" , "26/02/1984" , "Feminino"],
+   [ "Carlos Magalhaes de Carvalho", "335.467.657-12" , "93.657.620-76" , "05/10/1962" , "Masculino"],
+   [ "João Alcantra de Souza", "225.467.657-05" , "61.657.620-84" , "07/01/2005" , "Masculino"],
+   [ "Thiago Cordoba de Alcantra", "115.467.657-92" , "73.657.620-90" , "30/03/2007" , "Masculino"],
+   [ "Mauricio Antonio da Cruz", "005.467.657-73" , "87.657.620-06" , "06/11/1995" , "Masculino"],
+   [ "Perla Maria Bispo", "995.467.657-80" , "15.657.620-18" , "10/08/1980" , "Feminino"],
+]
+
+console.log("Nome Completo: " + dados[3][0] + " CPF "+ dados[3][1] + " RG: " + dados[3][2] + " Data de Nascimento: "  + dados[3][3] + " Sexo: " + dados[3][4] )
